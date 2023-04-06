8833c31d49a1fef4f8da5f4c61b9900504cff62d
Dylan Coxshall <dacoxshall@gmail.com>
2023-04-06 23:31:24 +0100

    Create board display method

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -20,14 +20,38 @@ impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut board_repr: [char; 64] = ['.'; 64];
 
-        write!(f, "{}", String::from_iter(board_repr))
+        // There has to be a better way of populating the array than this
+        board_repr = Board::populate_array(board_repr, 'P', &self.white_pawns);
+        board_repr = Board::populate_array(board_repr, 'R', &self.white_rooks);
+        board_repr = Board::populate_array(board_repr, 'N', &self.white_knights);
+        board_repr = Board::populate_array(board_repr, 'B', &self.white_bishops);
+        board_repr = Board::populate_array(board_repr, 'Q', &self.white_queens);
+        board_repr = Board::populate_array(board_repr, 'K', &self.white_king);
+        board_repr = Board::populate_array(board_repr, 'p', &self.black_pawns);
+        board_repr = Board::populate_array(board_repr, 'r', &self.black_rooks);
+        board_repr = Board::populate_array(board_repr, 'n', &self.black_knights);
+        board_repr = Board::populate_array(board_repr, 'b', &self.black_bishops);
+        board_repr = Board::populate_array(board_repr, 'q', &self.black_queens);
+        board_repr = Board::populate_array(board_repr, 'k', &self.black_king);
+
+        let mut string_board: String = "".to_string();
+
+        for i in 0..8 {
+            for j in 0..8 {
+                string_board.push(board_repr[i * 8 + j]);
+                string_board.push(' ');
+            }
+            string_board.push('\n');
+        }
+
+        write!(f, "{}", string_board)
     }
 }
 
 impl Board {
     fn populate_array(mut arr: [char; 64], piece: char, bitboard: &u64) -> [char; 64] {
         for i in 0..64 {
-            if (bitboard << i) & 1 == 1 {
+            if (bitboard >> i) & 1 == 1 {
                 arr[i] = piece;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use engine::Board;
 
 fn main() {
     let my_board = Board {
-        white_pawns: 5,
+        white_pawns: 0,
         white_rooks: 0,
         white_knights: 0,
         white_bishops: 0,
         white_queens: 0,
         white_king: 0,
-        black_pawns: 5,
+        black_pawns: 0,
         black_rooks: 0,
         black_knights: 0,
         black_bishops: 0,
