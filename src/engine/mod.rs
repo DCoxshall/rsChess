dc4329a0cd0856d71754ae6dbaafae15690e6bb3
Dylan Coxshall <dacoxshall@gmail.com>
2023-04-07 01:05:33 +0100

    Add FEN parsing function

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,19 +1,26 @@
 use std::fmt::Display;
 
 pub struct Board {
+    // Bitboards
     pub white_pawns: u64,
     pub white_rooks: u64,
     pub white_knights: u64,
     pub white_bishops: u64,
     pub white_queens: u64,
     pub white_king: u64,
-
     pub black_pawns: u64,
     pub black_rooks: u64,
     pub black_knights: u64,
     pub black_bishops: u64,
     pub black_queens: u64,
     pub black_king: u64,
+
+    // FEN information
+    pub turn: char,
+    pub castling_rights: String,
+    pub en_passant_target: u64,
+    pub half_move_clock: i32,
+    pub full_move_clock: i32,
 }
 
 impl Display for Board {
@@ -36,8 +43,8 @@ impl Display for Board {
 
         let mut string_board: String = "".to_string();
 
-        for i in 0..8 {
-            for j in 0..8 {
+        for i in (0..8).rev() {
+            for j in (0..8).rev() {
                 string_board.push(board_repr[i * 8 + j]);
                 string_board.push(' ');
             }
@@ -51,11 +58,76 @@ impl Display for Board {
 impl Board {
     fn populate_array(mut arr: [char; 64], piece: char, bitboard: &u64) -> [char; 64] {
         for i in 0..64 {
+            // Bit fuckery #1: perform right shift 64 times and AND against 1 to determine if there is a piece on this square.
             if (bitboard >> i) & 1 == 1 {
                 arr[i] = piece;
             }
         }
 
         arr
+    }
+
+    pub fn parse_fen(fen_string: &String) -> Board {
+        let fields: Vec<&str> = fen_string.split_whitespace().collect();
+        let mut new_board = Board {
+            white_pawns: 0,
+            white_rooks: 0,
+            white_knights: 0,
+            white_bishops: 0,
+            white_queens: 0,
+            white_king: 0,
+            black_pawns: 0,
+            black_rooks: 0,
+            black_knights: 0,
+            black_bishops: 0,
+            black_queens: 0,
+            black_king: 0,
+
+            turn: fields[1].chars().nth(0).unwrap(),
+            castling_rights: fields[2].to_string(),
+            en_passant_target: Board::convert_square_to_bitboard(&fields[3]),
+            half_move_clock: fields[4].to_string().parse::<i32>().unwrap(),
+            full_move_clock: fields[5].to_string().parse::<i32>().unwrap(),
+        };
+
+        let mut pos = 0;
+        for c in fields[0].chars() {
+            match c {
+                'P' => new_board.white_pawns |= 9223372036854775808 >> pos,
+                'R' => new_board.white_rooks |= 9223372036854775808 >> pos,
+                'N' => new_board.white_knights |= 9223372036854775808 >> pos,
+                'B' => new_board.white_bishops |= 9223372036854775808 >> pos,
+                'Q' => new_board.white_queens |= 9223372036854775808 >> pos,
+                'K' => new_board.white_king |= 9223372036854775808 >> pos,
+                'p' => new_board.black_pawns |= 9223372036854775808 >> pos,
+                'r' => new_board.black_rooks |= 9223372036854775808 >> pos,
+                'n' => new_board.black_knights |= 9223372036854775808 >> pos,
+                'b' => new_board.black_bishops |= 9223372036854775808 >> pos,
+                'q' => new_board.black_queens |= 9223372036854775808 >> pos,
+                'k' => new_board.black_king |= 9223372036854775808 >> pos,
+                '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' => pos += c.to_digit(10).unwrap(),
+                '/' => {}
+                _ => panic!(),
+            }
+            if String::from("PRNBQKprnbqk").contains(c) {
+                pos += 1;
+            }
+        }
+
+        new_board
+    }
+
+    fn convert_square_to_bitboard(square: &str) -> u64 {
+        if square.len() == 1 {
+            // square could only be '-', which means there is no valid en passant target
+            return 0;
+        }
+        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+        let square_file = square.chars().nth(0).unwrap();
+        let square_rank = square.chars().nth(1).unwrap();
+        let file_number: i32 = alphabet.iter().position(|&x| x == square_file).unwrap() as i32;
+        let rank_number = square_rank.to_string().parse::<i32>().unwrap() - 1;
+        return 9223372036854775808 >> (file_number * 8 + rank_number);
+        // 9223372036854775808 is the number which represents a 1 followed by 63 zeroes.
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,8 @@ mod engine;
 use engine::Board;
 
 fn main() {
-    let my_board = Board {
-        white_pawns: 0,
-        white_rooks: 0,
-        white_knights: 0,
-        white_bishops: 0,
-        white_queens: 0,
-        white_king: 0,
-        black_pawns: 0,
-        black_rooks: 0,
-        black_knights: 0,
-        black_bishops: 0,
-        black_queens: 0,
-        black_king: 0,
-    };
+    let my_board =
+        Board::parse_fen(&"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1".to_string());
 
     println!("{}", my_board);
 }
